6d2e1ddbc958597ed191b69327ebb1467e4276e7
ShiWenChen <cswzmg@live.com>
2017-02-10 15:43:14 +0800

    Initial Commit

---
+++ QRCodeCustom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeCustom
+//
+//  Created by admin on 2017/2/10.
+//  Copyright © 2017年 Rcfans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRCodeCustom/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QRCodeCustom
+//
+//  Created by admin on 2017/2/10.
+//  Copyright © 2017年 Rcfans. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
